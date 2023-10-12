f5a4f9eb04e37fdda6320fd077f362b2fb205bd4
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-10-12 14:55:56 +0300

    1.8.0-1

---
+++ Package.swift
@@ -0,0 +1,77 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let bnbPackageVersion: Version = "1.8.0-1"
+
+let package = Package(
+    name: "BNBFaceTracker",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BNBFaceTracker",
+            targets: ["BNBFaceTracker"]
+        ),
+        .library(
+            name: "BNBSdkCore",
+            targets: ["BNBSdkCore"]
+        ),
+        .library(
+            name: "BNBEffectPlayer",
+            targets: ["BNBEffectPlayer"]
+        ),
+        .library(
+            name: "BNBScripting",
+            targets: ["BNBScripting"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBSdkCore.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBEffectPlayer.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBScripting.git",
+            .exact(bnbPackageVersion)
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BNBFaceTracker",
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.8.0-58-g46079e7d74/BNBFaceTracker.zip",
+            checksum: "2fb17031ce6a0659819dea411dcbf574e8082361f14630494017f1568ccea802"
+        ),
+        .target(
+            name: "BNBSdkCore",
+            dependencies: [
+                .product(
+                    name: "BNBSdkCore",
+                    package: "BNBSdkCore"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBEffectPlayer",
+            dependencies: [
+                .product(
+                    name: "BNBEffectPlayer",
+                    package: "BNBEffectPlayer"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBScripting",
+            dependencies: [
+                .product(
+                    name: "BNBScripting",
+                    package: "BNBScripting"
+                ),
+            ]
+        ),
+    ]
+)
