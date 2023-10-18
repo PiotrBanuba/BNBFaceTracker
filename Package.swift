0fa2f61864bd89f58b43c5b21a199597b5417d5e
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-10-18 13:42:32 +0300

    1.8.0-63-g5825a46ccf

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.8.0-62-g077d3df291"
+let bnbPackageVersion: Version = "1.8.0-63-g5825a46ccf"
 
 let package = Package(
     name: "BNBFaceTracker",
@@ -36,8 +36,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBFaceTracker",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.8.0-62-g077d3df291/BNBFaceTracker.zip",
-            checksum: "d581df27bee248a041ac962055c8daa2c21a4defb5668a40f72e12bddc50f8b1"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.8.0-63-g5825a46ccf/BNBFaceTracker.zip",
+            checksum: "86fe915d1dac8acf52c495b315abd896b1a170d0b44daba1549f15a4327dfd1c"
         ),
         .target(
             name: "BNBFaceTracker_BNBSdkCore",
